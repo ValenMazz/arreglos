a589539ffa3a58034d3a260f27a92f0f147af9be
alumno <alumno@Instituto.local>
2025-05-21 08:20:44 -0300

    clase 21/05

---
+++ valen.js
@@ -0,0 +1,24 @@
+/*
+let frutas = ['banana', 'naranja', 'frutilla']; 
+
+console.log(frutas.length);
+
+frutas.push('mango')
+frutas.pop()
+frutas.shift()
+*/
+
+/*
+let nombres = ['benja', 'valen', 'juana']
+
+console.log(nombres.length)
+
+nombres.push('facu', 'alejo')
+*/
+
+let numeros = [1, 2, 3, 4, 5]
+
+for (let indice = 0; indice < numeros.length; index++) {
+    
+    
+}
