1f7c1e8a4871e1a63ae2c05374f6a1b25376d3a7
alumno <alumno@Instituto.local>
2025-05-22 11:21:59 -0300

    clase 22/05/25

--- valen.js
+++ valen.js
@@ -16,9 +16,20 @@ console.log(nombres.length)
 nombres.push('facu', 'alejo')
 */
 
+/*
 let numeros = [1, 2, 3, 4, 5]
 
 for (let indice = 0; indice < numeros.length; index++) {
-    
+    console.log()
     
 }
+*/
+
+let numero2 = [1, 2, 30, 4];
+let suma = 0;
+
+for (let index = 0; index < numero2.length; index++) {
+  suma = suma + numero2[index]
+}
+
+console.log(suma);
