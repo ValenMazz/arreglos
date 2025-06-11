1ce57890c857157ee26c5b8bd3c9fb9d68bb3d4f
alumno <alumno@Instituto.local>
2025-06-11 08:16:38 -0300

    clase 11/06

--- valen.js
+++ valen.js
@@ -25,6 +25,7 @@ for (let indice = 0; indice < numeros.length; index++) {
 }
 */
 
+/*
 let numero2 = [1, 2, 30, 4];
 let suma = 0;
 
@@ -33,3 +34,48 @@ for (let index = 0; index < numero2.length; index++) {
 }
 
 console.log(suma);
+*/
+
+let animales = ['gato', 'perro', 'loro', 'conejo']
+
+for (let i = 0; i < animales.length; i++) {
+  if (i % 2 === 0)
+  console.log( animales[i]);
+  
+}
+
+let numeros = [4, 7, 2, 9, 5];
+let suma = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+  suma += numeros[i];  // Se va sumando cada número al total
+}
+
+console.log("La suma es " + suma);
+
+let datos = [3, 6, 1, 8];
+
+let multiplicados = datos.map(num => num * 2);
+
+console.log(multiplicados);
+
+let alumnos = ["Sofía", "Carlos", "Bruno", "Lucía", "Carla"];
+let nombreBuscado = prompt("Introduce un nombre para buscar:");
+
+if (alumnos.includes(nombreBuscado)) {
+  console.log("El nombre se encontró en el arreglo.");
+} else {
+  console.log("El nombre no se encontró en el arreglo.");
+}
+
+let valores = [5, 0, 8, 0, 2];
+
+let nuevoArreglo = valores.map(num => num === 0 ? 100 : num);
+
+console.log(nuevoArreglo);
+
+let notas = [8, 7, 9, 10, 6];
+let suma1 = notas.reduce((acc, nota) => acc + nota, 0);
+let promedio = suma1 / notas.length;
+
+console.log("El promedio es " + promedio);
